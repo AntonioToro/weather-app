93c2670572843b7734b3d27e7fd33ae19a84ec65
AntonioToro <antoniotorolucas@gmail.com>
2022-10-11 15:30:49 +0200

    refactor: :art: Add useMemo in CityPage

--- src/pages/CityPage.jsx
+++ src/pages/CityPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import Grid from '@material-ui/core/Grid'
 import LinerProgress from '@material-ui/core/LinearProgress'
 import CityInfo from './../components/CityInfo'
@@ -15,7 +15,8 @@ import { getCountryNameByCountryCode } from '../utils/serviceCities'
 const CityPage = props => {
     const [city, countryCode, chartData, forecastItemList] = useCityPage()
 
-    const [ allWeather ] = useCityList([{ city, countryCode }])
+    const cities = useMemo(() => ([{ city, countryCode }]), [city, countryCode])
+    const [ allWeather ] = useCityList(cities)
 
     const weather = allWeather[getCityCode(city, countryCode)]
 
